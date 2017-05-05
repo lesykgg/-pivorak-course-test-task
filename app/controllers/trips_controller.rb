11945f6f38f78419db13563ea96fffa294719900
lesykgg <lesykgg@gmail.com>
2017-05-06 00:09:11 +0300

    Added trips, updated authentication

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,17 @@
+class TripsController < ApplicationController
+before_action :authenticate_user!
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+
+
+  root 'static_pages#home'
+
   devise_for :users
   get 'persons/profile'
 
-	root 'static_pages#home'
-	match 'signup', to: 'users#new', via: "get"
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :trips
 end
