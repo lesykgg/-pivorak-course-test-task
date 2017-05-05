fa77152e3ae56834adb98893b0f306fb1e2002b5
lesykgg <lesykgg@gmail.com>
2017-05-05 18:27:42 +0300

    Updated layouts

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
 gem 'pg'
 gem 'sass-rails', '~> 5.0'
+gem 'bootstrap-sass'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+require File.expand_path('../boot', __FILE__)
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -11,5 +13,6 @@ module TestTask
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
